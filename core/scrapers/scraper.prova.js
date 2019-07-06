4a191792f457d16a100a704cd23696de39e0e5fe
Andrea <andreamogavero.sa@gmail.com>
2019-07-06 14:09:26 +0200

    First sample of scraping has been updated

--- core/scrapers/scraper.prova.js
+++ core/scrapers/scraper.prova.js
@@ -3,7 +3,8 @@ const axios = require('axios');
 const DOMParser = require('xmldom').DOMParser;
 const xpath = require('xpath');
 
-axios.get(scraping.prova.url)
+async function getTime() {
+    axios.get(scraping.prova.url)
     .then( response => {
     // console.log(response.data);
     var doc = new DOMParser().parseFromString(response.data);
@@ -14,3 +15,17 @@ axios.get(scraping.prova.url)
     .catch(error => {
         console.log(error);
     })
+}
+
+// Testing
+getTime();
+
+/* EXPORTS */
+// // Choosing export name of the function
+// module.exports.ExportName = getTime;
+//         // or
+// module.exports = {
+//     getTime,
+//     ... other function ...,
+
+// }
