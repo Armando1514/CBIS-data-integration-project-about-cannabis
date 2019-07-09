639e7cb66c8d666975d7659a927a493dd4353206
Andrea <andreamogavero.sa@gmail.com>
2019-07-09 15:32:58 +0200

    Added mediator skeleton

--- app.js
+++ app.js
@@ -2,11 +2,8 @@ const express = require('express');
 const logger = require('loglevel');
 const config = require('./config/essential');
 const basicRoutes = require('./core/basicRoutes');
-// const ratingAnalyzerQuery = require('./core/query/ratingAnalyzerQuery')
-// const rentCountQuery = require('./core/query/rentCountQuery')
-// const squareMetresClassesQuery = require('./core/query/squareMetresClassesQuery')
-// const ratingClassesQuery = require('./core/query/ratingClassesQuery')
-// const amenityAndPropertyQuery = require('./core/query/amenityAndPropertyQuery')
+const showStrains = require('./core/mediator/showStrains');
+const showStrainInfo = require('./core/mediator/showStrainInfo');
 const connection = require('./core/db/connection');
 
 const app = express();
@@ -21,23 +18,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 
-app.get(config.basepath, basicRoutes.functionHomePage);
-// app.get(config.basepath + '/' + 'rentCount', basicRoutes.functionRentCount)
-// app.post(config.basepath + '/' + 'rentCountQuery', rentCountQuery)
-
-// app.get(config.basepath + '/' + 'amenityAndProperty', basicRoutes.functionAmenityAndProperty)
-// app.post(config.basepath + '/' + 'amenityAndPropertyQuery', amenityAndPropertyQuery)
-
-// app.get(config.basepath + '/' + 'ratingAnalyzer', basicRoutes.functionRatingAnalyzer)
-// app.post(config.basepath + '/' + 'ratingAnalyzerQuery', ratingAnalyzerQuery)
-
-// app.get(config.basepath + '/' + 'ratingClasses', basicRoutes.functionRatingClasses)
-// app.post(config.basepath + '/' + 'ratingClassesQuery', ratingClassesQuery)
-
-// app.get(config.basepath + '/' + 'squareMetresClasses', basicRoutes.functionSquareMetresClasses)
-// app.post(config.basepath + '/' + 'squareMetresClassesQuery', squareMetresClassesQuery)
-
-// app.get(config.basepath + '/' + 'about', basicRoutes.functionAboutPage)
+app.get(config.basepath, showStrains);
+app.get(config.basapath + '/' + 'strain', showStrainInfo);
 
 app.listen(config.port, config.host, () => logger.info('[System] App cbis has been deployed at: http://' + config.host + ':' + config.port + config.basepath));
 
